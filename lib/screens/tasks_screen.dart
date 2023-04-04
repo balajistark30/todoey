a0536aaabbbde63218d512009534aec7c9a679c0
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2023-04-04 09:36:48 +0200

    a style to the top column was added

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -9,9 +9,35 @@ class TasksScreen extends StatelessWidget {
           padding: const EdgeInsets.only(
               top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: const [
-              Icon(Icons.list),
-              Text('Todoey'),
+              CircleAvatar(
+                child: Icon(
+                  Icons.list,
+                  color: Colors.lightGreen,
+                  size: 30.0,
+                ),
+                backgroundColor: Colors.white,
+                radius: 30.0,
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                'Todoey',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 50.0,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Text(
+                '12 Tasks',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18.0,
+                ),
+              ),
             ],
           ),
         ));
