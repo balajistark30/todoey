9560d0aac118d97b80d6240e0042da61d95da9b9
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2023-04-04 11:39:22 +0200

    ListTitle was extracted to TaskTitle class

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -75,15 +75,26 @@ class TasksList extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView(
       children: const [
-        ListTile(
-          title: Text('This is a task'),
-          trailing: Checkbox(value: false, onChanged: null),
-        ),
+        TaskTile(),
         ListTile(
           title: Text('This is a task'),
           trailing: Checkbox(value: false, onChanged: null),
         ),
       ],
+    );
+  }
+}
+
+class TaskTile extends StatelessWidget {
+  const TaskTile({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text('This is a task'),
+      trailing: Checkbox(value: false, onChanged: null),
     );
   }
 }
