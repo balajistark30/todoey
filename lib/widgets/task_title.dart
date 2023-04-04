34b242b3649271b312caa3e40aeee0606e4ee9aa
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2023-04-04 13:18:02 +0200

    TaskCheckbox was extracted as StatefulWidget

--- lib/widgets/task_title.dart
+++ lib/widgets/task_title.dart
@@ -9,7 +9,23 @@ class TaskTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       title: Text('This is a task'),
-      trailing: Checkbox(value: false, onChanged: null),
+      trailing: TaskCheckbox(),
+    );
+  }
+}
+
+class TaskCheckbox extends StatefulWidget {
+  @override
+  _TaskCheckboxState createState() => _TaskCheckboxState();
+}
+
+class _TaskCheckboxState extends State<TaskCheckbox> {
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: true,
+      onChanged: null,
+      activeColor: Colors.lightGreen,
     );
   }
 }
