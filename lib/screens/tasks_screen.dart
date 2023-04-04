583fbc307e93f79d854fc6b2fa7a5c782aebd976
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2023-04-04 09:22:02 +0200

    top Column Widget was added

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
 class TasksScreen extends StatelessWidget {
-  const TasksScreen({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        backgroundColor: Colors.lightGreen,
+        body: Container(
+          padding: const EdgeInsets.only(
+              top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
+          child: Column(
+            children: const [
+              Icon(Icons.list),
+              Text('Todoey'),
+            ],
+          ),
+        ));
   }
 }
