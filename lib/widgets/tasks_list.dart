485cc1ad84a774ec8d3dc055de818d7a63042d94
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2023-04-05 11:38:06 +0200

    TasksList was converted to StatefulWidget

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart                                                              ';
 import 'package:todoey_mobile_app/widgets/task_title.dart';
 
-class TasksList extends StatelessWidget {
-  const TasksList({
-    super.key,
-  });
+class TasksList extends StatefulWidget {
+  @override
+  _TaskListState createState() => _TaskListState();
+}
 
+class _TaskListState extends State<TasksList> {
   @override
   Widget build(BuildContext context) {
     return ListView(
