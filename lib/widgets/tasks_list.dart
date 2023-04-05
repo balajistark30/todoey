665b7afc27203aaed2774e90ed8ccdd23e80d753
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2023-04-05 11:42:13 +0200

    Task class was created

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart                                                              ';
 import 'package:todoey_mobile_app/widgets/task_title.dart';
+import 'package:todoey_mobile_app/models/task.dart';
 
 class TasksList extends StatefulWidget {
   @override
@@ -7,6 +8,8 @@ class TasksList extends StatefulWidget {
 }
 
 class _TaskListState extends State<TasksList> {
+  List<Task> tasks = [];
+
   @override
   Widget build(BuildContext context) {
     return ListView(
